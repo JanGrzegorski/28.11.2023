a6c215fcff3b4d5aa8684ed04b6c878bfcde492a
pawelce <szkolacecot@gmail.com>
2023-11-28 10:35:42 +0100

    kiecka

---
+++ Zad2_losowanko/Zad2_losowanko.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <time.h>
+using namespace std;
+int main()
+{
+	srand(time(NULL));
+	clock_t start, stop;
+	start = clock();
+	cout << "Dynamiczne przydzielanie pamięci dla tablicy - WSKAZNIK" << endl;
+	int rozmiarTablicy;
+	int los;
+	cout << "Podaj rozmiar tablicy = "; cin >> rozmiarTablicy;
+	int* tablica;
+	tablica = new int[rozmiarTablicy];
+	for (int i = 0; i < rozmiarTablicy; i++) {
+		los = rand() % 100 + 1;
+		tablica[i] = los;
+	}
+
+	for (int i = 0; i < rozmiarTablicy; i++) {
+		cout << "Index nr " << i << " = " << tablica[i] << " Adres komorki: " << (int)&tablica[i] << endl;
+	}
+	stop = clock();
+	
+	double czaswyswietlaniaaplikacji = (double)(stop - start) / CLOCKS_PER_SEC;
+	cout << endl << czaswyswietlaniaaplikacji << endl;
+	delete[]tablica;
+	return EXIT_SUCCESS;
+}
---
+++ zad1/zad1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "");
+	int* tablica;
+	int ile = 5;
+	tablica = new int[ile];
+
+	cout << "Dynamiczne Adresy do przydzielenia 5 elementów = " << tablica << endl;
+	cout << "Adres własny wskaźnika tablica = " << (int)&tablica << endl;
+	cout << (int)&tablica[0] << endl;
+	cout << (int)&tablica[1] << endl;
+	cout << (int)&tablica[2] << endl;
+	cout << (int)&tablica[3] << endl;
+	cout << (int)&tablica[4] << endl;
+
+	//inny sposób wyświetlania adresów z tablicy
+	cout << endl << endl;
+	for (int i = 0; i < ile; i++)
+	{
+		cout << (int)tablica << endl;
+		tablica++;
+	}
+	//------------------------------
+	return 0;
+}
